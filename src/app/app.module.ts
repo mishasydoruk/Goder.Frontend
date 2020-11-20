f413e644808c763a69b40a3eacc7c37abe53e747
Ivan <gamick2002@gmail.com>
2020-11-20 21:20:51 +0200

    Removed already existed modules from app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,28 +11,10 @@ import { TokenInterceptorService } from './core/auth/token-interceptor.service';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MenuComponent } from './shared/menu/menu.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 
 @NgModule({
     declarations: [AppComponent, MenuComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        ScenesModule,
-        SharedModule,
-        CoreModule,
-        HttpClientModule,
-        LayoutModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        MatListModule,
-    ],
+    imports: [BrowserModule, AppRoutingModule, ScenesModule, SharedModule, CoreModule, HttpClientModule, LayoutModule],
     providers: [
         AuthService,
         {
