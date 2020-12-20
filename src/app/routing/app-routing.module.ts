7ed8aa70bb6565f2c478b340776984735edae266
Vitalii Yarmus <55307415+325Vitalik@users.noreply.github.com>
2020-12-20 16:18:53 +0200

    Add routing

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProblemsPageComponent } from '../scenes/components/problems-page/problems-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'problems',
+        component: ProblemsPageComponent,
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
